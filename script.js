bca74a2b26f489ba81125c0a7b1ef14448cc9f2e
Muhammad Nofil <nofil.khan037@gmail.com>
2023-06-14 14:28:58 +0500

    movie complete

---
+++ script.js
@@ -0,0 +1,18 @@
+function Movies() {
+    let Api = document.getElementById("input").value;
+    let ApiUrl = ` http://www.omdbapi.com/?t=${Api}&apikey=3ca6e694`;
+
+    getapi(ApiUrl);
+    async function getapi(url) {
+      let response = await fetch(url);
+      var data = await response.json();
+      show(data);
+    }
+    function show(data) {
+      document.getElementById("Title-1").innerHTML = data.Genre;
+      document.getElementById("Title-2").innerHTML = data.Released;
+      document.getElementById("Title-3").innerHTML = data.Runtime;
+      document.getElementById("Title-4").innerHTML = data.Writer;
+      document.getElementById("Title-5").innerHTML = data.Actors;
+    }
+  }
